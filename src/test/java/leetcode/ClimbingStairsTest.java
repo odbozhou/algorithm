c507c8b1c314e7bef748c4342db18b76150552cd
zhoubo <877036922@qq.com>
2017-04-16 23:36:59 +0800

    climbing stairs 问题

---
+++ src/test/java/leetcode/ClimbingStairsTest.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+import org.junit.Test;
+
+/**
+ * Created by 马佳 on 2017/4/16.
+ */
+public class ClimbingStairsTest {
+    @Test
+    public void solution() throws Exception {
+        System.out.println(ClimbingStairs.solution(5));
+    }
+
+}
