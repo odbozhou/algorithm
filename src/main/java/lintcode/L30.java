0f878c6aa0e653bf97fc85b64fe8a722ef62ccf7
zhoubo0404 <877036922@qq.com>
2017-11-02 11:38:39 +0800

    30、插入区间

--- src/main/java/lintcode/L30.java
+++ src/main/java/lintcode/L30.java
@@ -2,20 +2,41 @@ package lintcode;
 
 import lintcode.util.Interval;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * 插入区间
+ *
+ * @author zhoubo
+ */
 public class L30 {
     public List<Interval> insert(List<Interval> intervals, Interval newInterval) {
         if (null == newInterval) {
             return intervals;
         }
-        int i = 0;
- /*       intervals.
-        for(;i < intervals.size(); i++) {
-            intervals.get(i).start
-        }*/
-
-        return null;
+        intervals.add(newInterval);
+        Collections.sort(intervals, Comparator.comparingInt(o -> o.start));
+        List<Interval> result = new LinkedList<>();
+        Interval last = null;
+        for (Interval interval : intervals) {
+            if (null == last) {
+                last = interval;
+            }
+            if (result.isEmpty()) {
+                result.add(last);
+            } else {
+                if (last.end < interval.start) {
+                    result.add(interval);
+                    last = interval;
+                } else if (interval.end > last.end) {
+                    last.end = interval.end;
+                }
+            }
+        }
+        return result;
     }
 }
 
