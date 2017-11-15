b7f0e8a822fec1984528755ed6c1b7a23cc012e3
zhoubo0404 <877036922@qq.com>
2017-11-15 15:36:44 +0800

    L423 有效的括号序列

--- src/main/java/lintcode/L423.java
+++ src/main/java/lintcode/L423.java
@@ -5,7 +5,6 @@ import java.util.Stack;
 /**
  * 有效的括号序列
  * <p>
- * 错误"[({(())}[()])]"
  *
  * @author zhoubo
  * @create 2017-11-14 17:53
@@ -15,32 +14,10 @@ public class L423 {
         if (s.length() < 2 || s.length() % 2 != 0) {
             return false;
         }
-        boolean flag = false;
+        Stack<Character> characters = new Stack<>();
         char[] chars = s.toCharArray();
-        if (chars[0] == '(' && chars[1] == ')') {
-            flag = true;
-        } else if (chars[0] == '{' && chars[1] == '}') {
-            flag = true;
-        } else if (chars[0] == '[' && chars[1] == ']') {
-            flag = true;
-        } else {
-            flag = false;
-        }
-        if (flag) {
-            for (int i = 0; i < s.length() - 1; i += 2) {
-                if (chars[i] == '(' && chars[i + 1] == ')') {
-                    return true;
-                } else if (chars[i] == '{' && chars[i + 1] == '}') {
-                    return true;
-                } else if (chars[i] == '[' && chars[i + 1] == ']') {
-                    return true;
-                } else {
-                    return false;
-                }
-            }
-        } else {
-            Stack<Character> characters = new Stack<>();
-            int i = 0;
+        int i = 0;
+        while (i < s.length() - 1) {
             for (; i < s.length() - 1; i++) {
                 characters.push(chars[i]);
                 if (chars[i] == '(' && chars[i + 1] == ')') {
@@ -51,48 +28,20 @@ public class L423 {
                     break;
                 }
             }
-            while (i < s.length() && !characters.empty()) {
-                if (chars[i] == ')' && !characters.pop().equals('(')) {
-                    return false;
-                } else if (chars[i] == '}' && !characters.pop().equals('{')) {
-                    return false;
-                } else if (chars[i] == ']' && !characters.pop().equals('[')) {
-                    return false;
+            while (i < s.length() - 1 && !characters.empty()) {
+                if (characters.peek().equals('(') && chars[i + 1] != ')') {
+                    break;
+                } else if (characters.peek().equals('{') && chars[i + 1] != '}') {
+                    break;
+                } else if (characters.peek().equals('[') && chars[i + 1] != ']') {
+                    break;
                 }
+                characters.pop();
                 i++;
-            }
-            if (i != s.length() || !characters.empty()) {
-                return false;
-            }
-            return true;
-        }
-        return false;
-    }
-
-    public boolean isValidParentheses1(String s) {
-        Stack<Character> characters = new Stack<>();
-        char[] chars = s.toCharArray();
-        int i = 0;
-        for (; i < s.length() - 1; i++) {
-            characters.push(chars[i]);
-            if (chars[i] == '(' && chars[i + 1] == ')') {
-                break;
-            } else if (chars[i] == '{' && chars[i + 1] == '}') {
-                break;
-            } else if (chars[i] == '[' && chars[i + 1] == ']') {
-                break;
-            }
-        }
-        while (i < s.length() && !characters.empty()) {
-            if (chars[i] == ')' && !characters.pop().equals('(')) {
-                return false;
-            } else if (chars[i] == '}' && !characters.pop().equals('{')) {
-                return false;
-            } else if (chars[i] == ']' && !characters.pop().equals('[')) {
-                return false;
             }
             i++;
         }
+
         if (i != s.length() || !characters.empty()) {
             return false;
         }
---
+++ src/test/java/lintcode/L423Test.java
@@ -0,0 +1,15 @@
+package lintcode;
+
+import org.junit.Test;
+
+/**
+ * @author zhoubo
+ * @create 2017-11-15 11:32
+ */
+public class L423Test {
+    @Test
+    public void isValidParentheses() throws Exception {
+        System.out.println(new L423().isValidParentheses("()[]{}"));
+    }
+
+}
