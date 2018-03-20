6daf577fa5b05a306a8241ae823548de5c0eaa89
zhoubo0404 <877036922@qq.com>
2018-03-20 14:45:32 +0800

    测试是否会发消息到钉钉

---
+++ src/main/java/EnumTest.java
@@ -0,0 +1,14 @@
+public enum EnumTest {
+    SPRING("spring"), SUMMER, AUTUMN, WINTER;
+
+    private String value;
+
+    EnumTest(String value) {
+        this.value = value;
+    }
+
+    EnumTest() {
+
+    }
+}
+
